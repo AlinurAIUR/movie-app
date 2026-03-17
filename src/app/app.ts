c479af00440c5c05df92a331da6ce35053689626
AlinurAIUR <yedilalinur@gmail.com>
2026-03-17 13:15:12 +0500

    added search and footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,11 +3,12 @@ import {Header} from './header/header';
 import { MainComponent} from './main/main';
 import {FavoritesComponent} from './favorites/favorites';
 import {RouterOutlet} from '@angular/router';
+import {FooterComponent} from './footer/footer';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
-  imports: [Header, RouterOutlet],
+  imports: [Header, RouterOutlet, FooterComponent],
   styleUrls: ['./app.css'],
 })
 export class AppComponent {}
--- src/app/main/main.ts
+++ src/app/main/main.ts
@@ -1,31 +1,73 @@
-import {Component, OnInit, signal} from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Environment } from '../data/environments/environment';
-import {CommonModule} from '@angular/common';
-import {FavoritesService} from '../services/favorites.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { FavoritesService } from '../services/favorites.service';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './main.html',
   styleUrls: ['./main.css'],
 })
 export class MainComponent implements OnInit {
 
   movies = signal<any[]>([]);
+  currentPage = signal<number>(1);
+  totalPages = signal<number>(1);
+  searchQuery = signal<string>('');
+  isSearching = signal<boolean>(false); // флаг поиска
 
   constructor(
-    private http: HttpClient,
-    public favoritesService: FavoritesService
+      private http: HttpClient,
+      public favoritesService: FavoritesService
   ) {}
 
   ngOnInit() {
+    this.loadMovies();
+  }
+
+  loadMovies() {
     this.http.get<any>(
-      `https://api.themoviedb.org/3/movie/popular?api_key=${Environment.tmdbApiKey}&language=ru-RU&page=1`
+        `https://api.themoviedb.org/3/movie/popular?api_key=${Environment.tmdbApiKey}&language=ru-RU&page=${this.currentPage()}`
     ).subscribe((response: any) => {
       this.movies.set(response.results);
+      this.totalPages.set(response.total_pages);
     });
+  }
+
+  searchMovies() {
+    const query = this.searchQuery().trim();
+    if (!query) {
+      this.isSearching.set(false);
+      this.currentPage.set(1);
+      this.loadMovies();
+      return;
+    }
+
+    this.isSearching.set(true);
+    this.http.get<any>(
+        `https://api.themoviedb.org/3/search/movie?api_key=${Environment.tmdbApiKey}&language=ru-RU&query=${query}&page=${this.currentPage()}`
+    ).subscribe((response: any) => {
+      this.movies.set(response.results);
+      this.totalPages.set(response.total_pages);
+    });
+  }
+
+  nextPage() {
+    if (this.currentPage() < this.totalPages()) {
+      this.currentPage.update(page => page + 1);
+      this.isSearching() ? this.searchMovies() : this.loadMovies();
+    }
+  }
+
+  prevPage() {
+    if (this.currentPage() > 1) {
+      this.currentPage.update(page => page - 1);
+      this.isSearching() ? this.searchMovies() : this.loadMovies();
+    }
   }
 
   addToFavorites(movie: any) {
